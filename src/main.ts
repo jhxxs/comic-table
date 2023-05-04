856b36d55a48c259d5558e2908ca09c34d8d6109
Kyle <iamjhxxs@gmail.com>
2023-05-04 14:39:39 +0800

    feat: 使用暗黑模式

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from "./App.vue"
 import "./assets/style.css"
 import ElementPlus from "element-plus"
 import zhCn from "element-plus/dist/locale/zh-cn.mjs"
+import "element-plus/theme-chalk/dark/css-vars.css"
 
 const app = createApp(App)
 
