1e64ba874948250e195e1be5652e9dc262c524c5
Kyle <iamjhxxs@gmail.com>
2023-05-04 15:32:27 +0800

    feat: 移除谷歌分析；使用vercel分析

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import "./assets/style.css"
 import ElementPlus from "element-plus"
 import zhCn from "element-plus/dist/locale/zh-cn.mjs"
 import "element-plus/theme-chalk/dark/css-vars.css"
+import { inject } from "@vercel/analytics"
+
+inject()
 
 const app = createApp(App)
 
